719f128f792172bbe2fedb63ce0b973ed5ff91fb
unknown <odil2006@gmail.com>
2015-08-03 22:51:11 +0500

    first add files

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="background-image:url(images/background/fon_1.jpg); height:190px;">
+
+ 
+    <div class="container">
+
+      <div class="starter-template">
+    	
+    	<br>
+		<div style="background-image:url(images/way_1.jpg); background-repeat:repeat-x; height:190px;">
+				<img src="cars/bugatti.png" style="margin-top: 19px;">
+		</div>
+    	
+
+    	<br><br>
+		<div style="background-image:url(images/way_1.jpg); background-repeat:repeat-x; height:190px;">
+				<img src="cars/ferrari.png" style="margin-top: 19px;">
+		</div>
+
+
+    	<br><br>
+		<div style="background-image:url(images/way_1.jpg); background-repeat:repeat-x; height:190px;">
+				<img src="cars/lamborjini.png" style="margin-top: 19px;">
+		</div>
+				
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    
+  </body>
+</html>

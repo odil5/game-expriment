e21fcd05ecfafb33b67f86f35a1d24740eae7a6a
unknown <odil2006@gmail.com>
2015-10-18 22:20:54 +0500

    add small animation

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -9,10 +9,10 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Starter Template for Bootstrap</title>
+    <title>Racing</title>
 
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
 
 
@@ -25,26 +25,36 @@
 
   <body style="background-image:url(images/background/fon_1.jpg); height:190px;">
 
- 
+	<br/>
+	<div class="text-center"><a href="#" class="btn btn-default" id="start">Start</a> <a href="#" id="reset" class="btn btn-default">Reset</a></div>
     <div class="container">
 
       <div class="starter-template">
     	
     	<br>
-		<div style="background-image:url(images/way_1.jpg); background-repeat:repeat-x; height:190px;">
-				<img src="cars/bugatti.png" style="margin-top: 19px;">
+		<div>
+			<span style="color:#fff">420</span>
+			<div style="background-image:url(/images/way_1.jpg); background-repeat:repeat-x; height:190px;">
+					<img class="car" src="/cars/bugatti.png" style="margin-top: 19px;">
+			</div>
 		</div>
     	
 
     	<br><br>
-		<div style="background-image:url(images/way_1.jpg); background-repeat:repeat-x; height:190px;">
-				<img src="cars/ferrari.png" style="margin-top: 19px;">
+		<div>
+			<span style="color:#fff">350</span>
+			<div style="background-image:url(/images/way_1.jpg); background-repeat:repeat-x; height:190px;">
+					<img class="car" src="/cars/ferrari.png" style="margin-top: 19px;">
+			</div>
 		</div>
 
 
     	<br><br>
-		<div style="background-image:url(images/way_1.jpg); background-repeat:repeat-x; height:190px;">
-				<img src="cars/lamborjini.png" style="margin-top: 19px;">
+		<div>
+			<span style="color:#fff">250</span>
+			<div style="background-image:url(/images/way_1.jpg); background-repeat:repeat-x; height:190px;">
+					<img class="car" src="/cars/lamborjini.png" style="margin-top: 19px;">
+			</div>
 		</div>
 				
       </div>
@@ -55,9 +65,23 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    
+     <script>
+		$(function(){
+			$("#start").on("click", function(e){				
+				e.preventDefault();
+				$(".car").each(function(i){
+					$(this).stop().animate({"margin-left":"828px"}, 4000*(i+1));
+				});
+			});
+			
+			$("#reset").on("click", function(e){
+				e.preventDefault();
+				$(".car").css("margin-left", "0");
+			});
+		});
+	</script>
   </body>
 </html>
